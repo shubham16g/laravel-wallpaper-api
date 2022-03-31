d9d166e3622fd07aeb5dfde4c8dcda37e9f4b35f
Shubham Gupta <shubham.developer16@gmail.com>
2022-03-31 14:20:39 +0530

    Cache for 15 minutes

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::middleware('api.user')->group(function () {
     Route::post('/wall/list/', [WallController::class, 'list']);
     Route::middleware('cache.headers:public;max_age=43200')->get('/list/{type}', [AllTagController::class, 'index']);
     Route::middleware('cache.headers:public;max_age=43200')->get('/init', [AllTagController::class, 'init']);
-    Route::middleware('cache.headers:public;max_age=300')->get('/wall/', [WallController::class, 'index']);
+    Route::middleware('cache.headers:public;max_age=900')->get('/wall/', [WallController::class, 'index']);
 });
 
 Route::middleware('api.admin')->group(function () {
