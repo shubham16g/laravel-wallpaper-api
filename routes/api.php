db7c9c47cc8b4c3853a7a4b5aa657cc4243e6305
Shubham Gupta <shubham.developer16@gmail.com>
2022-04-20 22:57:27 +0530

    base cache age decreased

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::middleware('api.user')->group(function () {
     Route::middleware('cache.headers:public;max_age=43200')->get('/list/{type}', [AllTagController::class, 'index']);
     Route::middleware('cache.headers:public;max_age=43200')->get('/init', [AllTagController::class, 'init']);
     Route::middleware('cache.headers:public;max_age=900')->get('/wall/', [WallController::class, 'index']);
-    Route::middleware('cache.headers:public;max_age=43200')->get('/base', [WallController::class, 'base']);
+    Route::middleware('cache.headers:public;max_age=7200')->get('/base', [WallController::class, 'base']);
 });
 
 Route::middleware('api.admin')->group(function () {
